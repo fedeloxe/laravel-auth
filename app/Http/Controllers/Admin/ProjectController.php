5cc45a969e8b3d9223e8e12a30fdd11eb5a0f097
fedeloxe <loxeee@gmail.com>
2023-03-01 18:20:11 +0100

    create, store done

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -27,7 +27,8 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+
+        return view('admin.projects.create');
     }
 
     /**
@@ -38,7 +39,11 @@ class ProjectController extends Controller
      */
     public function store(StoreProjectRequest $request)
     {
-        //
+        $new = $request->all();
+        $slug = Project::generateSlug($request->title);
+        $new['slug'] = $slug;
+        Project::create($new);
+        return redirect()->route('admin.projects.index');
     }
 
     /**
